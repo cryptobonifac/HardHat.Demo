ad165a8e494a67e90e70f2cce91f390121419410
cryptobonifac <maro10@protonmail.com>
2023-05-03 13:08:37 +0200

    completed lession 6

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+
+contract HelloWorld {
+    string private text;
+    address public owner;
+
+    constructor() {
+        text = "Hello World";
+        owner = msg.sender;
+    }
+
+    function helloWorld() public view returns (string memory) {
+        return text;
+    }
+
+    function setText(string calldata newText) public onlyOwner {
+        text = newText;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    modifier onlyOwner()
+    {
+        require (msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+}
